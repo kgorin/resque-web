b58b1cf20c7b54f27bf8545973d0b1677e5bba7f
kgorin <machineshopflow@gmail.com>
2015-03-20 21:20:01 +0300

    Added different sort_by values for workers list
    run_at, queue, worker_name, job_class

--- app/helpers/resque_web/working_helper.rb
+++ app/helpers/resque_web/working_helper.rb
@@ -12,8 +12,25 @@ module ResqueWeb
       @worker_jobs ||= workers.zip(jobs).reject { |w, j| w.idle? || j['queue'].nil? }
     end
 
-    def sorted_worker_jobs
-      @sorted_worker_jobs ||= worker_jobs.sort_by { |w, j| j['run_at'] || '' }
+    def sorted_worker_jobs(sort_by = 'run_at', desc = false)
+      @sorted_worker_jobs ||= sorted_by(worker_jobs, sort_by, desc)
+    end
+
+  private
+
+    def sorted_by(worker_jobs, sort_by, desc)
+      res = case sort_by
+      when 'run_at'
+        worker_jobs.sort_by { |w, j| j['run_at'] || '' }
+      when 'queue'
+        worker_jobs.sort_by { |w, j| j['queue'] || '' }
+      when 'worker_name'
+        worker_jobs.sort_by { |w, j| w.to_s || '' }
+      when 'job_class'
+        worker_jobs.sort_by { |w, j| j['payload']['class'] || '' }
+      end
+
+      desc ? res.reverse : res
     end
   end
 end
